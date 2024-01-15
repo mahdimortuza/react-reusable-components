36f7a04591b47a53b2a713ed77cf1f1b1d07e974
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-15 09:10:08 +0600

    reusable tailwind class

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import MainLayout from "./components/layout/MainLayout";
+import Container from "./components/ui/Container";
 
 function App() {
-  return <MainLayout />;
+  return (
+    <Container>
+      <h1 className="">Main container</h1>
+      <button className="btn btn-primary">click</button>
+      <button className="btn btn-danger">click</button>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/ui/Container.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+const Container = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="w-full h-full max-w-[1050px] mx-auto bg-red-500">
+      {children}
+    </div>
+  );
+};
+
+export default Container;
