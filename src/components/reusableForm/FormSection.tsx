544b71292b8cc04c71a32d5ebfb21a89d64d898d
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-17 12:43:20 +0600

    form section and form submit created

---
+++ src/components/reusableForm/FormSection.tsx
@@ -0,0 +1,9 @@
+const FormSection = () => {
+  return (
+    <div>
+      <h1>form section</h1>
+    </div>
+  );
+};
+
+export default FormSection;
---
+++ src/components/reusableForm/FormSubmit.tsx
@@ -0,0 +1,9 @@
+const FormSubmit = () => {
+  return (
+    <div>
+      <h1>form submit</h1>
+    </div>
+  );
+};
+
+export default FormSubmit;
